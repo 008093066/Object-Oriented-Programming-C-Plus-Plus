20b83ceeebc183df94138f220d15521d39f6ea95
008093066 <008093066@coyote.csusb.edu>
2024-02-05 20:15:18 -0800

    corrected program code

--- Lab 1/Hello.cpp
+++ Lab 1/Hello.cpp
@@ -1,8 +1,14 @@
-#include <iostream>
+/***
+ * Familiarize self with IDE, Github, CLI, and compiling/executing C++ programs
+***/
 
+#include <iostream>
+#include <cstdlib>
 using namespace std;
 
 int main() 
 {
-    cout << "Hello World!\n";
+    cout << "Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
 }
